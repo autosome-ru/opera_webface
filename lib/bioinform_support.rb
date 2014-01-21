1b2fb71ef79bb2b9743a6b6250030ffa7412d984
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-01-21 14:59:05 +0400

    pcm/ppm don't need background

--- lib/bioinform_support.rb
+++ lib/bioinform_support.rb
@@ -27,13 +27,16 @@ module Bioinform
     pm.to_pwm
   end
 
-  def self.get_pcm(data_model, matrix, background, effective_count)
+  def self.get_pcm(data_model, matrix, effective_count)
     pm = Bioinform.const_get(data_model).new(matrix)
-    pm.set_parameters(background: background)
     if effective_count && [:PPM].include?(data_model.to_sym)
       pm.set_parameters(effective_count: effective_count)
     end
     pm.to_pcm
+  end
+
+  def self.get_ppm(data_model, matrix)
+    Bioinform.const_get(data_model).new(matrix).to_ppm
   end
 
 end
