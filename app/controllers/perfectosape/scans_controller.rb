fea9cdb00b7494b947b10c011d223ba9fbdc83d7
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-02-14 00:39:02 +0400

    extra spaces removed from example

--- app/controllers/perfectosape/scans_controller.rb
+++ app/controllers/perfectosape/scans_controller.rb
@@ -3,10 +3,10 @@ class Perfectosape::ScansController < ::TasksController
 
   def default_params
     test_snps = <<-EOS
-rs10040172  gatttgccctgattgcagttactga[G/A]tggtacagacatcgtaataatctta
-rs10116271  taaattctatgtggggaagaggtct[C/T]gtagaggcgatgattcttacattgc
-rs10208293  cttcatacatttatgtccagtacct[A/G]tggaccctccttgtgaactcttctc
-rs10431961  tggcggggctggtcaggcggcgtcg[C/T]cggtacgctctgagcggcagcgtgt
+rs10040172 gatttgccctgattgcagttactga[G/A]tggtacagacatcgtaataatctta
+rs10116271 taaattctatgtggggaagaggtct[C/T]gtagaggcgatgattcttacattgc
+rs10208293 cttcatacatttatgtccagtacct[A/G]tggaccctccttgtgaactcttctc
+rs10431961 tggcggggctggtcaggcggcgtcg[C/T]cggtacgctctgagcggcagcgtgt
     EOS
 
     { snp_list: test_snps,
