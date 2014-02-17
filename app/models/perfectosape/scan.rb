d52aab6fd006e24dc17d68500bc8caca33e73c1d
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-02-17 18:59:01 +0400

    small style fixes

--- app/models/perfectosape/scan.rb
+++ app/models/perfectosape/scan.rb
@@ -1,7 +1,8 @@
 class Perfectosape::Scan < ::Task
   add_task_params :snp_list
   add_task_params :collection
-  add_task_params :pvalue_cutoff, :fold_change_cutoff, &:to_f
+  add_task_params :pvalue_cutoff, &:to_f
+  add_task_params :fold_change_cutoff, &:to_f
   add_background_task_param :background
 
   extend Enumerize
