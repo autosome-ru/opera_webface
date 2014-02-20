60096fbc01c339bd5a06e3fbde2ebf2c7d92d805
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-02-20 04:25:56 +0400

    remove unnecessary class_eval (make less tabs ;))

--- app/models/concerns/background_parameters.rb
+++ app/models/concerns/background_parameters.rb
@@ -6,18 +6,16 @@ module BackgroundParameters
 
   module ClassMethods
     def add_background_task_param(param_name)
-      class_eval do
-        param_name = param_name.to_sym
-        add_task_submission_param(param_name){|task, value| value.background }
-        add_task_permitted_param("#{param_name}_attributes" => [:mode, :gc_content, :frequencies_attributes => [:a,:c,:g,:t] ])
+      param_name = param_name.to_sym
+      add_task_submission_param(param_name){|task, value| value.background }
+      add_task_permitted_param("#{param_name}_attributes" => [:mode, :gc_content, :frequencies_attributes => [:a,:c,:g,:t] ])
 
-        attr_reader param_name
+      attr_reader param_name
 
-        define_method "#{param_name}_attributes=" do |value|
-          instance_variable_set("@#{param_name}", Background.new(value))
-        end
-        add_deep_validation_for(param_name)
+      define_method "#{param_name}_attributes=" do |value|
+        instance_variable_set("@#{param_name}", Background.new(value))
       end
+      add_deep_validation_for(param_name)
     end
   end
 end
