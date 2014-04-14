9f8b11313cf17063cbb4870f73efaaed2ec150fb
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-04-14 21:32:00 +0400

    macroape/scan add uniprot link

--- app/helpers/macroape/scans_helper.rb
+++ app/helpers/macroape/scans_helper.rb
@@ -1,12 +1,11 @@
 module Macroape::ScansHelper
   def scan_collection_table(task_results, task_params)
     collection_name = task_params.collection
-
     header = results_header_from_txt(task_results) + ['logo']
     lines = results_data_from_txt(task_results)
     lines = lines.map do |line|
       motif, similarity, shift, overlap, orientation, precise_mode = *line
-      motif_info = motif + '<br>' + collection_motif_links(collection_name, motif)
+      motif_info = motif + '<br>' + collection_motif_links(collection_name, motif) + '<br>' + uniprot_links(collection_name, motif)
       logo_path = collection_motif_image(collection_name, motif, orientation)
       [motif_info, similarity.to_f.round(5), shift, overlap, orientation, precise_mode, logo_path]
     end
@@ -16,4 +15,5 @@ module Macroape::ScansHelper
     end
     create_table(header, lines, table_html: {class: 'colorized'})
   end
+
 end
