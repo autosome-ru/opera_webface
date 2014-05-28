3ac6dc4852d53c1c2c02f75b5a9666f4a68925eb
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-05-28 16:29:11 +0400

    Query motif with aligned logo at the top of macroape/scan results

--- app/helpers/macroape/scans_helper.rb
+++ app/helpers/macroape/scans_helper.rb
@@ -1,5 +1,5 @@
 module Macroape::ScansHelper
-  def scan_collection_table(task_results, task_params)
+  def scan_collection_table(task_results, task_params, ticket)
     collection_name = task_params.collection
     header = results_header_from_txt(task_results) + ['logo']
     lines = results_data_from_txt(task_results)
@@ -13,6 +13,7 @@ module Macroape::ScansHelper
       motif_info, similarity, shift, overlap, orientation, precise_mode, logo_path = *line
       1.0 - similarity
     end
+    lines.unshift ['<span class="query_motif">Query</span>', 'N/A', 0, 'N/A', 'N/A', 'N/A', show_image(ticket, 'query.png')]  if SMBSMCore.check_content(ticket, 'query.png')
     create_table(header, lines, table_html: {class: 'colorized macroape_scan_results'})
   end
 
