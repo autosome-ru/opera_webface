f0da040b4daf85d99fd2bfa037ffc4da1e5b310a
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-07-15 05:40:54 +0400

    fix

--- app/models/data_model.rb
+++ app/models/data_model.rb
@@ -3,72 +3,6 @@ require 'enumerize'
 require 'bioinform'
 require_relative 'background'
 
-module OperaWebface
-  module DataModelPresenter
-    def self.get_model(data_model, matrix, name)
-      Bioinform::MotifModel.const_get(data_model).new(matrix).named(name)
-    end
-
-    def self.get_model_from_string(data_model, matrix_string)
-      motif_infos = Bioinform::MatrixParser.new.parse(matrix_string)
-      get_model(data_model, motif_infos[:matrix], motif_infos[:name])
-    end
-
-    def self.get_pwm(data_model, matrix, background, pseudocount, effective_count)
-      # background = (background == [1,1,1,1]) ? Bioinform::Background.wordwise : Bioinform::Frequencies.new(background)
-      background = background.value
-      input_model = get_model_from_string(data_model, matrix)
-      if Bioinform::MotifModel.acts_as_ppm?(input_model)
-        ppm2pcm_converter = Bioinform::ConversionAlgorithms::PPM2PCMConverter.new(count: effective_count)
-        pcm2pwm_converter = Bioinform::ConversionAlgorithms::PCM2PWMConverter.new(background: background, pseudocount: pseudocount || :log)
-        pcm2pwm_converter.convert(ppm2pcm_converter.convert(input_model))
-      elsif Bioinform::MotifModel.acts_as_pcm?(input_model)
-        pcm2pwm_converter = Bioinform::ConversionAlgorithms::PCM2PWMConverter.new(background: background, pseudocount: pseudocount || :log)
-        pcm2pwm_converter.convert(input_model)
-      elsif Bioinform::MotifModel.acts_as_pwm?(input_model)
-        input_model
-      else
-        raise Bioinform::Error, "Unknown input `#{input_model}`"
-      end
-    rescue => e
-      raise Bioinform::Error, "PWM creation failed (#{e})"
-    end
-
-    def self.get_pcm(data_model, matrix, effective_count)
-      input_model = get_model_from_string(data_model, matrix)
-      if Bioinform::MotifModel.acts_as_ppm?(input_model)
-        ppm2pcm_converter = Bioinform::ConversionAlgorithms::PPM2PCMConverter.new(count: effective_count)
-        ppm2pcm_converter.convert(input_model)
-      elsif Bioinform::MotifModel.acts_as_pcm?(input_model)
-        input_model
-      elsif Bioinform::MotifModel.acts_as_pwm?(input_model)
-        raise Bioinform::Error, 'Conversion PWM-->PCM not yet implemented'
-      else
-        raise Bioinform::Error, "Unknown input `#{input_model}`"
-      end
-    rescue => e
-      raise Bioinform::Error, "PCM creation failed (#{e})"
-    end
-
-    def self.get_ppm(data_model, matrix)
-      input_model = get_model_from_string(data_model, matrix)
-      if Bioinform::MotifModel.acts_as_ppm?(input_model)
-        input_model
-      elsif Bioinform::MotifModel.acts_as_pcm?(input_model)
-        pcm2ppm_converter = Bioinform::ConversionAlgorithms::PCM2PPMConverter.new
-        pcm2ppm_converter.convert(input_model)
-      elsif Bioinform::MotifModel.acts_as_pwm?(input_model)
-        raise Bioinform::Error, 'Conversion PWM-->PPM not yet implemented'
-      else
-        raise Bioinform::Error, "Unknown input `#{input_model}`"
-      end
-    rescue => e
-      raise Bioinform::Error, "PPM creation failed (#{e})"
-    end
-  end
-end
-
-
 class DataModel
   include ActiveModel::Model
   extend Enumerize
@@ -78,14 +12,23 @@ class DataModel
   attr_accessor :matrix, :background
   attr_reader :data_model, :effective_count, :pseudocount
 
+  # def self.get_model(data_model, matrix, name)
+  #   Bioinform::MotifModel.const_get(data_model).new(matrix).named(name)
+  # end
+
+  # def self.get_model_from_string(data_model, matrix_string)
+  #   motif_infos = Bioinform::MatrixParser.new.parse(matrix_string)
+  #   get_model(data_model, motif_infos[:matrix], motif_infos[:name])
+  # end
+
   def to_hash
     result = {matrix: matrix, data_model: data_model, effective_count: effective_count, pseudocount: pseudocount, background: background.to_hash}
-    case data_model
-    when :PWM
-      result.merge(pwm: pwm.to_s)
-    else
-      result.merge(pwm: pwm.to_s, pcm: pcm.to_s, ppm: ppm.to_s)
-    end#.tap{|x|p x}
+    # case data_model
+    # when :PWM
+    #   result.merge(pwm: pwm.to_s)
+    # else
+    #   result.merge(pwm: pwm.to_s, pcm: pcm.to_s, ppm: ppm.to_s)
+    # end#.tap{|x|p x}
   end
 
   def data_model=(value)
@@ -105,37 +48,63 @@ class DataModel
     end
   end
 
+  def motif
+    Bioinform::MotifModel.const_get(data_model).from_string(matrix)
+  end
+
   def pwm
-    OperaWebface::DataModelPresenter.get_pwm(data_model, matrix, background, pseudocount, effective_count)
+    # OperaWebface::DataModelPresenter.get_pwm(data_model, matrix, background, pseudocount, effective_count)
+    background = background.value
+    # input_model = self.class.get_model_from_string(data_model, matrix)
+    input_model = motif
+    if Bioinform::MotifModel.acts_as_ppm?(input_model)
+      ppm2pcm_converter = Bioinform::ConversionAlgorithms::PPM2PCMConverter.new(count: effective_count)
+      pcm2pwm_converter = Bioinform::ConversionAlgorithms::PCM2PWMConverter.new(background: background, pseudocount: pseudocount || :log)
+      pcm2pwm_converter.convert(ppm2pcm_converter.convert(input_model))
+    elsif Bioinform::MotifModel.acts_as_pcm?(input_model)
+      pcm2pwm_converter = Bioinform::ConversionAlgorithms::PCM2PWMConverter.new(background: background, pseudocount: pseudocount || :log)
+      pcm2pwm_converter.convert(input_model)
+    elsif Bioinform::MotifModel.acts_as_pwm?(input_model)
+      input_model
+    else
+      raise Bioinform::Error, "Unknown input `#{input_model}`"
+    end
+  rescue => e
+    raise Bioinform::Error, "PWM creation failed (#{e})"
   end
 
   def pcm
-    case data_model
-    when :PCM, :PPM
-      OperaWebface::DataModelPresenter.get_pcm(data_model, matrix, effective_count)
+    # input_model = self.class.get_model_from_string(data_model, matrix)
+    input_model = motif
+    if Bioinform::MotifModel.acts_as_ppm?(input_model)
+      ppm2pcm_converter = Bioinform::ConversionAlgorithms::PPM2PCMConverter.new(count: effective_count)
+      ppm2pcm_converter.convert(input_model)
+    elsif Bioinform::MotifModel.acts_as_pcm?(input_model)
+      input_model
+    elsif Bioinform::MotifModel.acts_as_pwm?(input_model)
+      raise Bioinform::Error, 'Conversion PWM-->PCM not yet implemented'
     else
-      nil
+      raise Bioinform::Error, "Unknown input `#{input_model}`"
     end
+  rescue => e
+    raise Bioinform::Error, "PCM creation failed (#{e})"
   end
 
   def ppm
-    case data_model
-    when :PCM, :PPM
-      OperaWebface::DataModelPresenter.get_ppm(data_model, matrix)
+    # input_model = self.class.get_model_from_string(data_model, matrix)
+    input_model = motif
+    if Bioinform::MotifModel.acts_as_ppm?(input_model)
+      input_model
+    elsif Bioinform::MotifModel.acts_as_pcm?(input_model)
+      pcm2ppm_converter = Bioinform::ConversionAlgorithms::PCM2PPMConverter.new
+      pcm2ppm_converter.convert(input_model)
+    elsif Bioinform::MotifModel.acts_as_pwm?(input_model)
+      raise Bioinform::Error, 'Conversion PWM-->PPM not yet implemented'
     else
-      nil
+      raise Bioinform::Error, "Unknown input `#{input_model}`"
     end
-  end
-
-  def data_model_object
-    case data_model
-    when :PWM
-      pwm
-    when :PCM
-      pcm
-    when :PPM
-      ppm
-    end
+  rescue => e
+    raise Bioinform::Error, "PPM creation failed (#{e})"
   end
 
   validates :effective_count, presence: true, numericality: {greater_than: 0}, :if => ->(model){ model.data_model == :PPM }
@@ -147,7 +116,7 @@ class DataModel
     # # because model evaluation with broken background is impossible
     if record.background.valid?
       begin
-        record.errors.add(:matrix, record.data_model_object.validation_errors.join(";\n"))  unless record.data_model_object#.valid?
+        record.errors.add(:matrix, record.motif.validation_errors.join(";\n"))  unless record.motif #.valid?
       rescue
         record.errors.add(:matrix, 'is invalid')
       end
