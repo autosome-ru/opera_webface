07b01910a0ff6fe8d5c46fc10b27510c53893845
VorontsovIE <vorontsov.i.e@gmail.com>
2016-04-22 21:40:24 +0300

    parameter description now looks up localization through model hierarchy

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -10,8 +10,11 @@ module TasksHelper
       all_errors += object.errors.full_messages_for(attribute)  if all_errors.empty?
       additional_options[:error] = all_errors.join(";\n")  unless all_errors.empty?
 
+      defaults = object.class.ancestors.drop(1).select(&:name).map{|klass|
+        ['task_parameters', *klass.name.split('::'), attribute].compact.reject(&:empty?).join('.').to_sym
+      }
       i18n_path = options[:i18n_description] || ['task_parameters', task_type, attribute].compact.join('.')
-      additional_options[:parameter_description] = I18n.t(i18n_path, default: "Description not provided (#{i18n_path})")
+      additional_options[:parameter_description] = I18n.t(i18n_path, default: [*defaults, "Description not provided (#{i18n_path})"])
      
       options[:input_html] ||= {}
       options[:input_html][:data] ||= {}
