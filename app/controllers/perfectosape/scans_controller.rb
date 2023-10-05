cc28cebf2d9de864b4f34fdfad077501370fa1a4
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2023-10-05 11:36:44 +0300

    perfectos by default uses H12RSNP collection

--- app/controllers/perfectosape/scans_controller.rb
+++ app/controllers/perfectosape/scans_controller.rb
@@ -14,7 +14,7 @@ rs10431961 tggcggggctggtcaggcggcgtcg[C/T]cggtacgctctgagcggcagcgtgt
     EOS
 
     { snp_list_text: test_snps,
-      collection: :hocomoco_12_core,
+      collection: :hocomoco_12_rsnp,
       pvalue_cutoff: 0.0005,
       fold_change_cutoff: 4
     }
