b14d647b9d60a57da261058a63f2475f019ec5d2
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2026-01-12 13:56:56 +0300

    rails 5 → 6

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,7 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link_directory ../javascripts .js
+
+// if using importmaps, you may also need:
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
